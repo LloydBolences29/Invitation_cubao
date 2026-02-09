aea69c6134244627c603a27863ec91c10aed4060
LloydBolences29 <lmbolences@gmail.com>
2026-02-09 23:17:50 +0800

    added some styles

--- simpleProject/src/component/MoreDetails.jsx
+++ simpleProject/src/component/MoreDetails.jsx
@@ -1,35 +1,42 @@
-import React from 'react';
-import { Calendar, Shirt, MapPin } from 'lucide-react';
+import React from "react";
+import { Calendar, Shirt, MapPin } from "lucide-react";
 
 const MoreDetails = () => {
   return (
-    <div className='more-details-container'>
+    <div className="more-details-container">
+      <div className="more-details-header">
+        <h3>Can I invite you to a Date?</h3>
+      </div>
       <div className="more-details-wrapper">
-        
         <div className="details-card animate-card-1">
-          <div className="card-icon"><Calendar size={32} strokeWidth={1.2} /></div>
-          <h3>The When</h3>
-          <p className="main-info">February 14, 2026</p>
-          <p className="sub-info">Saturday • 3:00 PM</p>
+          <div className="card-icon">
+            <Calendar size={32} strokeWidth={1.2} />
+          </div>
+          <h3>When?</h3>
+          <p className="main-info">February 22, 2026</p>
+          <p className="sub-info">Sunday • 9am until 4pm</p>
         </div>
 
         <div className="details-card animate-card-2">
-          <div className="card-icon"><Shirt size={32} strokeWidth={1.2} /></div>
+          <div className="card-icon">
+            <Shirt size={32} strokeWidth={1.2} />
+          </div>
           <h3>The Look</h3>
           <p className="main-info">Anything with White</p>
           <p className="sub-info">Light, airy, and comfy</p>
         </div>
 
         <div className="details-card animate-card-3">
-          <div className="card-icon"><MapPin size={32} strokeWidth={1.2} /></div>
-          <h3>The Where</h3>
-          <p className="main-info">Cubao Expo</p>
-          <p className="sub-info">Coffee & Curiosities</p>
+          <div className="card-icon">
+            <MapPin size={32} strokeWidth={1.2} />
+          </div>
+          <h3>Where?</h3>
+          <p className="main-info">Around Cubao</p>
+          <p className="sub-info">Coffee & sunset merienda</p>
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MoreDetails;
